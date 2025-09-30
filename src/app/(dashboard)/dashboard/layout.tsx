4ca817c1eb58dd14ed262c620e3314e0cc839b4a
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-09-30 16:20:15 +0600

    dashboard sidebar added

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,22 @@
-export default function DashboardLayout() {
+// app/dashboard/layout.tsx
+
+import { AppSidebar } from "@/components/modules/dashboard/AppSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div>
-      <h1>This is Dashboard layout component</h1>
+    <div className="flex min-h-screen">
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex-1 bg-background p-6">
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
